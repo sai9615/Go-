df9d3e4458c3701c796ceb511210ee67a0643dae
sai milind <sai9615@gmail.com>
2022-02-22 19:43:09 -0600

    BinarySearch First Occurence of Element

---
+++ DS and Algo/BinarySearchFirstOccurence/BinarySearchFirstOccurence.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func findFirstOccurrence(arr []int, target int) int {
+	if len(arr) == 0 {
+		return -1
+	}
+	start := 0
+	end := len(arr) - 1
+	index := -1
+	for start <= end {
+		mid := start + (end-start)/2
+		if arr[mid] == target {
+			index = mid
+			end = mid - 1
+		} else if arr[mid] <= target {
+			start = mid + 1
+		} else {
+			end = mid - 1
+		}
+	}
+	return index
+}
+
+func arrayAtoi(arr []string) []int {
+	res := []int{}
+	for _, x := range arr {
+		v, _ := strconv.Atoi(x)
+		res = append(res, v)
+	}
+	return res
+}
+
+func splitWords(s string) []string {
+	if s == "" {
+		return []string{}
+	}
+	return strings.Split(s, " ")
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	arr := arrayAtoi(splitWords(scanner.Text()))
+	scanner.Scan()
+	target, _ := strconv.Atoi(scanner.Text())
+	res := findFirstOccurrence(arr, target)
+	fmt.Println(res)
+}
